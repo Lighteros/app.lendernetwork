48faf669698343d5d2fe0fa3df20893ec0947d14
Lighteros <lightningdev722@gmail.com>
2025-01-13 03:39:56 -0500

    fix

--- _next/static/chunks/app/layout-b57a56ee63ae6597.js
+++ _next/static/chunks/app/layout-b57a56ee63ae6597.js
@@ -93,8 +93,8 @@
             },
           ],
           y = [
-            { label: "\uD835\uDD4F", icon: b.LCd, href: "https://x.com/LND_Network" },
-            { label: "Telegram", icon: f.AGi, href: "https://t.me/LND_Network" },
+            { label: "\uD835\uDD4F", icon: b.LCd, href: "https://x.com/LNDNetworkETH" },
+            { label: "Telegram", icon: f.AGi, href: "https://t.me/LNDNetworkETH" },
           ];
         return (0, n.jsxs)("div", {
           children: [
